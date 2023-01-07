1cf0994137a2456db35ce94815fae1ba422f012a
alan <83582613+alannlol@users.noreply.github.com>
2023-01-07 09:08:59 +0000

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,1 +1,12 @@
--- Coming Soon
+local supported = {
+    8888615802,
+    6516141723,
+}
+
+local repo = "https://raw.githubusercontent.com/alannlol/redware/main/Games/"
+
+if table.find(supported, game.PlaceId) then
+    local gid = table.find(supported, game.PlaceId)
+
+    loadstring(game:HttpGet(repo..gid..".lua"))()
+end
